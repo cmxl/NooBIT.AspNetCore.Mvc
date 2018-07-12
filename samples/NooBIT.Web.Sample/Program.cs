65b54f74dcbf7328004582b255180670b9dded53
Tobias Haimerl <tobias.haimerl@conceptnet.de>
2018-07-12 22:26:08 +0200

    add sample project

---
+++ samples/NooBIT.Web.Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NooBIT.Web.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hostBuilder = new WebHostBuilder()
+                .UseKestrel(x => x.AddServerHeader = false)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .SuppressStatusMessages(false)
+                .UseIISIntegration()
+                .UseStartup<Startup>();
+
+            var host = hostBuilder.Build();
+            host.Run();
+        }
+    }
+}
