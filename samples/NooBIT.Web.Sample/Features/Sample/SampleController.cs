370c98b0c95e509d35510f5fdd95db21a5d5ad32
tobias.haimerl <tobias.haimerl@conceptnet.de>
2018-07-13 15:10:17 +0200

    rework sample to reflect more feature from startup

---
+++ samples/NooBIT.Web.Sample/Features/Sample/SampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NooBIT.Web.Sample.Features.Sample
+{
+    public class SampleController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = $"Hello from {nameof(SampleController)}!";
+            return View();
+        }
+    }
+}
